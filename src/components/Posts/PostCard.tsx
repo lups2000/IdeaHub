ea17fb180530368d1a1dd41061a0aa4d2d971dd8
Matteo Luppi <100372313+lups2000@users.noreply.github.com>
2024-09-28 10:36:36 +0200

    add first prototype grid of posts

--- src/components/Posts/PostCard.tsx
+++ src/components/Posts/PostCard.tsx
@@ -2,33 +2,57 @@ import downVoteIcon from "../../assets/down_vote.svg";
 import upVoteIcon from "../../assets/up_vote.svg";
 import commentsIcon from "../../assets/comments.svg";
 
-export const PostCard = () => {
+export interface PostCardProps {
+  username: string;
+  date: string;
+  title: string;
+  imageUrl?: string;
+  likes: number;
+  comments: number;
+}
+
+export const PostCard = (props: PostCardProps) => {
+  const { username, date, title, imageUrl, likes, comments } = props;
+
   return (
-    <div className="flex flex-col p-4 bg-white shadow-md rounded-lg space-y-2 max-w-lg w-full">
+    <div className="flex flex-col p-4 bg-white shadow-md rounded-lg space-y-2 max-w-lg w-full h-full">
       <div className="flex items-center space-x-2 text-sm text-gray-500">
-        <div className="font-semibold">username</div>
+        <div className="font-semibold">{username}</div>
         <div>&bull;</div>
-        <div>date</div>
+        <div>{date}</div>
       </div>
-      <div className="text-lg font-bold text-gray-800">Title of the Post</div>
-      <div className="w-full bg-gray-200 flex items-center justify-center rounded-lg">
-        <span className="text-gray-500">Image Placeholder</span>
-      </div>
-      <div className="flex flex-row gap-4">
+      <div className="text-lg font-bold text-gray-800">{title}</div>
+      {imageUrl ? undefined : (
+        <figure>
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            alt="Shoes"
+            className="rounded-xl"
+          />
+        </figure>
+      )}
+
+      {/* Spacer to push content to the bottom */}
+      <div className="flex-grow"></div>
+
+      <div className="pt-4 border-t mt-4 flex flex-row gap-2">
         <div className="flex items-center space-x-2 bg-gray-100 p-1 rounded-full">
           <button className="flex items-center justify-center w-8 h-8 hover:bg-gray-200 rounded-full">
             <img src={upVoteIcon} alt="Upvote" className="w-4 h-4" />
           </button>
-          <div className="text-sm font-medium text-gray-700">130</div>
+          <div className="text-sm font-medium text-gray-700">{likes}</div>
           <button className="flex items-center justify-center w-8 h-8 hover:bg-gray-200 rounded-full">
             <img src={downVoteIcon} alt="Downvote" className="w-4 h-4" />
           </button>
         </div>
+
         <div className="flex items-center space-x-1 bg-gray-100 p-1 rounded-full">
           <button className="flex items-center justify-center w-8 h-8 hover:bg-gray-200 rounded-full">
-            <img src={commentsIcon} alt="Upvote" className="w-4 h-4" />
+            <img src={commentsIcon} alt="Comments" className="w-4 h-4" />
           </button>
-          <div className="text-sm font-medium text-gray-700 p-1">130</div>
+          <div className="text-sm font-medium text-gray-700 p-1">
+            {comments}
+          </div>
         </div>
       </div>
     </div>
--- src/components/Posts/PostsContainer.tsx
+++ src/components/Posts/PostsContainer.tsx
@@ -1,9 +1,55 @@
-import { PostCard } from "./PostCard";
+import { PostCard, PostCardProps } from "./PostCard";
 
 export const PostsContainer = () => {
+  const posts: PostCardProps[] = [
+    {
+      username: "u/Direct_Message_8558",
+      date: "11h ago",
+      title:
+        "Who has most successfully milked what should have been 15 minutes of fame?",
+      imageUrl: undefined,
+      likes: 5300,
+      comments: 4200,
+    },
+    {
+      username: "u/VioEnvy",
+      date: "19h ago",
+      title: "I woke up to a black glove in my foyer.",
+      imageUrl: undefined,
+      likes: 3200,
+      comments: 2400,
+    },
+    {
+      username: "u/PopsicleDragon",
+      date: "19h ago",
+      title: "Do you agree with him? (Former PlayStation Boss Message)",
+      imageUrl: "/path-to-image/ghost_of_tsushima.jpg",
+      likes: 19800,
+      comments: 3300,
+    },
+    {
+      username: "u/Mint_Perspective",
+      date: "16h ago",
+      title:
+        "A blimp crashes into buildings in a Sao Paulo suburb on Wednesday, Sept. 25th",
+      imageUrl: undefined,
+      likes: 40500,
+      comments: 3300,
+    },
+    {
+      username: "u/emily-is-happy",
+      date: "3h ago",
+      title: "They don't care about US (Bezos meme)",
+      imageUrl: "/path-to-image/bezos_meme.jpg",
+      likes: 8400,
+      comments: 497,
+    },
+  ];
   return (
-    <div className="flex items-start bg-gray-100 p-10 min-h-screen">
-      <PostCard />
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 p-10 bg-gray-100 min-h-screen">
+      {posts.map((post, index) => (
+        <PostCard key={index} {...post} />
+      ))}
     </div>
   );
 };
