90fe56fab91171c9873ea789b2bb5e5e989967a8
Matteo Luppi <100372313+lups2000@users.noreply.github.com>
2024-09-28 08:19:22 +0200

    create first skeleton navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
+import { Navbar } from "./components/Navbar";
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">Hello World</h1>
+    <div>
+      <Navbar />
+    </div>
   );
 }
 
