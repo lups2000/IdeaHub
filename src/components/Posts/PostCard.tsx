8a2342adea02b41b2881d5e87cd6c0a5a10632fa
Matteo Luppi <100372313+lups2000@users.noreply.github.com>
2024-09-30 10:58:18 +0200

    add filled icons for likes

--- src/components/Posts/PostCard.tsx
+++ src/components/Posts/PostCard.tsx
@@ -1,5 +1,7 @@
 import downVoteIcon from "../../assets/down_vote.svg";
 import upVoteIcon from "../../assets/up_vote.svg";
+import upVoteIconFilled from "../../assets/up_vote_filled.svg";
+import downVoteIconFilled from "../../assets/down_vote_filled.svg";
 import commentsIcon from "../../assets/comments.svg";
 
 export interface PostCardProps {
@@ -39,18 +41,40 @@ export const PostCard = (props: PostCardProps) => {
       <div className="flex-grow"></div>
 
       <div className="pt-4 border-t mt-4 flex flex-row gap-2">
-        <div className="flex items-center bg-gray-100 p-1 rounded-full">
-          <button className="flex items-center justify-center w-8 h-8 hover:bg-gray-200 rounded-full">
-            <img src={upVoteIcon} alt="Upvote" className="w-4 h-4" />
-          </button>
+        <div className="flex items-center space-x-2 bg-gray-100 p-1 px-2 rounded-full">
+          <label className="swap">
+            {/* this hidden checkbox controls the state */}
+            <input type="checkbox" />
+            {/* upVote icon empty */}
+            <img src={upVoteIcon} alt="Upvote" className="w-4 h-4 swap-off" />
+            {/* upVote icon filled */}
+            <img
+              src={upVoteIconFilled}
+              alt="Upvote"
+              className="w-4 h-4 swap-on"
+            />
+          </label>
+
           <div className="text-sm font-medium text-gray-700">{likes}</div>
-          <button className="flex items-center justify-center w-8 h-8 hover:bg-gray-200 rounded-full">
-            <img src={downVoteIcon} alt="Downvote" className="w-4 h-4" />
-          </button>
+          <label className="swap">
+            {/* this hidden checkbox controls the state */}
+            <input type="checkbox" />
+            {/* downVote icon empty */}
+            <img src={downVoteIcon} alt="Downvote" className="w-4 h-4 swap-off" />
+            {/* downVote icon filled */}
+            <img
+              src={downVoteIconFilled}
+              alt="Downvote"
+              className="w-4 h-4 swap-on"
+            />
+          </label>
         </div>
 
         <div className="flex items-center bg-gray-100 p-1 rounded-full">
-          <button className="flex items-center justify-center w-8 h-8 rounded-full" disabled>
+          <button
+            className="flex items-center justify-center w-8 h-8 rounded-full"
+            disabled
+          >
             <img src={commentsIcon} alt="Comments" className="w-4 h-4" />
           </button>
           <div className="text-sm font-medium text-gray-700 p-1">
