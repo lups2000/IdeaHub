d6ee0c1424f0104f3e8ffeb5df574977923a2708
Matteo Luppi <100372313+lups2000@users.noreply.github.com>
2024-10-01 16:00:56 +0200

    message in case of no comments

--- src/components/Posts/PostEngagement/PostCommentsContainer.tsx
+++ src/components/Posts/PostEngagement/PostCommentsContainer.tsx
@@ -14,7 +14,7 @@ export const PostCommentsContainer = ({
   postId,
 }: PostCommentsContainerProps) => {
   const [comments, setComments] = useState<PostCommentInterface[]>([]);
-  
+
   useEffect(() => {
     getCommentsPost("/reactjs", postId)
       .then((response) => setComments(response))
@@ -25,14 +25,18 @@ export const PostCommentsContainer = ({
     <div className="flex flex-col mt-2">
       <div className="text-xl font-semibold">Comments</div>
       <div className="mt-2">
-        {comments.map((comment) => (
-          <PostComment
-            key={comment.data.id}
-            username={comment.data.author}
-            date={comment.data.created_utc}
-            comment={comment.data.body_html}
-          />
-        ))}
+        {comments.length > 0 ? (
+          comments.map((comment) => (
+            <PostComment
+              key={comment.data.id}
+              username={comment.data.author}
+              date={comment.data.created_utc}
+              comment={comment.data.body_html}
+            />
+          ))
+        ) : (
+          <p className="text-gray-500">There are no comments yet!</p>
+        )}
       </div>
     </div>
   );
