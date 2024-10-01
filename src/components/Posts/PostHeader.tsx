4a1326391402fbef16eef11fbca69e9bc481efca
Matteo Luppi <100372313+lups2000@users.noreply.github.com>
2024-10-01 12:08:50 +0200

    reduce space header

--- src/components/Posts/PostHeader.tsx
+++ src/components/Posts/PostHeader.tsx
@@ -9,7 +9,7 @@ export const PostHeader = ({ username, date }: PostHeaderProps) => {
   const formattedDate = formatDate(date);
 
   return (
-    <div className="flex items-center space-x-2 text-sm text-gray-500">
+    <div className="flex items-center space-x-1 text-sm text-gray-500">
       <div className="font-semibold">{username}</div>
       <div>&bull;</div>
       <div>{formattedDate}</div>
