e73a3e4a86d40b234e181dbb47314f89bbca6e26
Matteo Luppi <100372313+lups2000@users.noreply.github.com>
2024-10-02 14:44:14 +0200

    remove profile dropdown

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,9 @@ export const Navbar = () => {
   return (
     <div className="navbar bg-base-100 shadow-lg z-10 relative">
       <div className="flex-1">
-        <a href="/" className="btn btn-ghost text-xl">IdeaHub</a>
+        <a href="/" className="btn btn-ghost text-xl">
+          IdeaHub
+        </a>
       </div>
       <div className="dropdown dropdown-end">
         <div
@@ -17,23 +19,6 @@ export const Navbar = () => {
             />
           </div>
         </div>
-        <ul
-          tabIndex={0}
-          className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-        >
-          <li>
-            <a className="justify-between">
-              Profile
-              <span className="badge">New</span>
-            </a>
-          </li>
-          <li>
-            <a>Settings</a>
-          </li>
-          <li>
-            <a>Logout</a>
-          </li>
-        </ul>
       </div>
     </div>
   );
